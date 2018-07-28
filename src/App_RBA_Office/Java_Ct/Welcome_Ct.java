54999300dc7a061323360599615056c6a53a1993
rahulbaradol <rahulbaradol142@gmail.com>
2018-07-28 19:38:07 +0530

    Beta of RBA Office - Professional which is not ready yet!!

---
+++ src/App_RBA_Office/Java_Ct/Welcome_Ct.java
@@ -0,0 +1,115 @@
+package App_RBA_Office.Java_Ct;
+
+import javafx.animation.*;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+import javafx.util.Duration;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Welcome_Ct implements Initializable {
+
+    @FXML
+    private StackPane welcome;
+
+    @FXML
+    private Label heading, description;
+
+    private boolean clicked = false;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        welcome.setOpacity(0);
+
+        FadeTransition ft = new FadeTransition(Duration.seconds(2), welcome);
+        ft.setFromValue(0);
+        ft.setToValue(1);
+        ft.play();
+
+        zoomDescriptionOutside();
+    }
+
+    private void zoomDescriptionOutside() {
+        RotateTransition rt_1 = new RotateTransition(Duration.seconds(1), description);
+        ScaleTransition st_1 = new ScaleTransition(Duration.seconds(1), description);
+        ParallelTransition pt_1 = new ParallelTransition(rt_1, st_1);
+
+        rt_1.setToAngle(360);
+        st_1.setToX(2);
+        st_1.setToY(2);
+
+        pt_1.setOnFinished(event -> zoomDescriptionInside());
+
+        pt_1.play();
+    }
+
+    private void zoomDescriptionInside() {
+        RotateTransition rt_1 = new RotateTransition(Duration.seconds(1), description);
+        ScaleTransition st_1 = new ScaleTransition(Duration.seconds(1), description);
+        ParallelTransition pt = new ParallelTransition(rt_1, st_1);
+
+        rt_1.setToAngle(-20);
+        st_1.setToX(1);
+        st_1.setToY(1);
+
+        pt.setOnFinished(event -> zoomDescriptionOutside());
+
+        pt.play();
+    }
+
+    public void load_RBA_Office_Pro() {
+        if (!clicked) {
+            TranslateTransition tt_D = new TranslateTransition(Duration.seconds(1.5), description);
+            TranslateTransition tt_H = new TranslateTransition(Duration.seconds(1.5), heading);
+
+            ScaleTransition st_D = new ScaleTransition(Duration.seconds(1.5), description);
+            ScaleTransition st_H = new ScaleTransition(Duration.seconds(1.5), heading);
+
+            ParallelTransition pt = new ParallelTransition(tt_D, tt_H, st_D, st_H);
+
+            tt_D.setToX(600);
+            tt_H.setToX(-600);
+
+            st_D.setToX(-0.1);
+            st_D.setToY(-0.1);
+
+            st_H.setToX(-0.1);
+            st_H.setToY(-0.1);
+
+            pt.play();
+
+            pt.setOnFinished(event -> {
+                FadeTransition ft = new FadeTransition(Duration.seconds(1), welcome);
+                ft.setFromValue(1);
+                ft.setToValue(0);
+
+                ft.setOnFinished(event_FT -> {
+                    try {
+                        Stage stage = (Stage) welcome.getScene().getWindow();
+                        FXMLLoader loader = new FXMLLoader(getClass().getResource("/App_RBA_Office/FXML_Files/Rahul_Baradols_Application_Office.fxml"));
+                        Scene sc = new Scene(loader.load());
+
+                        stage.setTitle("Rahul Baradol's Application Office - Professional");
+                        stage.setScene(sc);
+                        stage.setResizable(false);
+                        stage.show();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                });
+
+                ft.play();
+            });
+
+            clicked = true;
+        }
+    }
+
+}
---
+++ src/App_RBA_Office/Rahul_Baradols_Application_Office_MC.java
@@ -0,0 +1,44 @@
+package App_RBA_Office;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.stage.Stage;
+
+public class Rahul_Baradols_Application_Office_MC extends Application {
+
+    @Override
+    public void start(Stage ps) {
+        try {
+            Scene sc = new Scene(FXMLLoader.load(getClass().getResource("/App_RBA_Office/FXML_Files/Welcome.fxml")));
+            sc.getStylesheets().setAll("/App_RBA_Office/Design/Design.css");
+
+            ps.setTitle("Welcome to RBA Office - Professional");
+            ps.setScene(sc);
+            ps.setResizable(false);
+            ps.show();
+        } catch (Exception ep) {
+            Alert error = new Alert(Alert.AlertType.ERROR);
+
+            error.setTitle("RBA Office");
+            error.setContentText("Error loading the file. \nCould not find the file");
+            error.showAndWait();
+        }
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void stop() {
+        Alert exit = new Alert(Alert.AlertType.INFORMATION);
+
+        exit.setTitle("RBA Office - Professional");
+        exit.setHeaderText(null);
+        exit.setContentText("THANKS for using RBA(Rahul Baradol's Application) Office. \nHave a nice day. See you soon.");
+        exit.showAndWait();
+    }
+
+}
